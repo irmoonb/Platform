3698dbdfccf62697dda2a4743096e38e944c7aa8
Irmoon B <irmoonb@Irmoons-MacBook-Air.local>
2025-01-28 15:37:11 -0600

    gravity, and other cool variables

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
-import math
-import random
-import tty
 import pygame
 import sys
+import random
+import math
+
 
 class Character(pygame.sprite.Sprite):
     def __init__(self, x, y, color):
+        """
+        This function is to initialize the character with all of its attributes.
+        This inludes the appearance, and a variable initialized for checking if
+        it's on the floor. If not, apply gravity.
+
+        :param x:
+        :param y:
+        :param color:
+        """
         super(Character, self).__init__()
         self.image = pygame.Surface((100, 100), pygame.SRCALPHA, 32)
         self.image.convert_alpha()
         self.image.fill(color)
         self.rect = self.image.get_rect(center=(x, y))
+        self.on_ground = False
 
     def move(self, deltax, deltay):
-        if self.rect.left < 0 or self.rect.right > 1200:
-            deltax *= -3
-        if self.rect.top < 0 or self.rect.bottom > 600:
-            deltay *= -3
+        """
+        This function enables the character to be moved.
 
-        self.rect.centerx += deltax
-        self.rect.centery += deltay
+        :param deltax:
+        :param deltay:
+        :return:
+        """
+        self.rect.x += deltax
+        self.rect.y += deltay
+
+    def gravity(self):
+        """
+        This function enables for gravity to be applied to the character,
+        IF they are not on a surface.
+
+        :return:
+        """
+        if not self.on_ground:
+            self.velocity_y += 1
+            self.rect.y += self.velocity_y
 
 
 # Initialize Pygame and give access to all the methods in the package
@@ -34,9 +57,11 @@ pygame.display.set_caption("Agario")
 # Create clock to later control frame rate
 clock = pygame.time.Clock()
 
-sq1 = Character(100, 300, "red")
+sq1 = Character(1100, 300, "red")
+sq2 = Character(100, 300, "pink")
 characters = pygame.sprite.Group()
 characters.add(sq1)
+characters.add(sq2)
 
 # Main game loop
 running = True
@@ -50,14 +75,23 @@ while running:
     # Get the state of all keys
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a]:
+    if keys[pygame.K_LEFT]:
         sq1.move(-2, 0)
-    if keys[pygame.K_d]:
+    if keys[pygame.K_RIGHT]:
         sq1.move(2, 0)
-    if keys[pygame.K_w]:
+    if keys[pygame.K_UP]:
         sq1.move(0, -2)
-    if keys[pygame.K_s]:
+    if keys[pygame.K_DOWN]:
         sq1.move(0, 2)
+
+    if keys[pygame.K_a]:
+        sq2.move(-2, 0)
+    if keys[pygame.K_d]:
+        sq2.move(2, 0)
+    if keys[pygame.K_w]:
+        sq2.move(0, -2)
+    if keys[pygame.K_s]:
+        sq2.move(0, 2)
 
     characters.draw(screen)
 
